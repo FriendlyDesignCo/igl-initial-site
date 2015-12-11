7160b95f67e753e90a2d1a26537908ad85ff6431
Ross Nover <ross@friendlydesign.co>
2015-12-11 16:26:42 -0500

    fixed random images

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+var headers         = ["h1","h2","h3"];                
+var rand = Math.floor(Math.random()*headers.length);           
+$('.top').addClass(headers[rand]);
+//
+//
+var footers         = ["f1","f2","f3"];                
+var rand = Math.floor(Math.random()*footers.length);           
+$('.mid').addClass(footers[rand]);
+
